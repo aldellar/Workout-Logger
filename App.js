8ffd5b2a01baecdfc73c17be83de9bc6e15ee893
rdwelter <127368877+rdwelter@users.noreply.github.com>
2024-07-07 12:16:24 -0700

    Make app scrollable

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 import Dotw from './components/Dotw'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Dotw day="Monday" goal="My goal" />
-      <Dotw day="Tuesday" goal="My goal" />
-      <Dotw day="Wednesday" goal="My goal" />
-      <Dotw day="Thursday" goal="My goal" />
-      <Dotw day="Friday" goal="My goal" />
-      <Dotw day="Saturday" goal="My goal" />
-      <Dotw day="Sunday" goal="My goal" />
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
+        <Dotw day="Monday" goal="My goal" />
+        <Dotw day="Tuesday" goal="My goal" />
+        <Dotw day="Wednesday" goal="My goal" />
+        <Dotw day="Thursday" goal="My goal" />
+        <Dotw day="Friday" goal="My goal" />
+        <Dotw day="Saturday" goal="My goal" />
+        <Dotw day="Sunday" goal="My goal" />
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
