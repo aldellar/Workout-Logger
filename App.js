d636a1c7640c44f98623864a8804b3a4b0333f05
Ryan Welter <rdwelter@ucsc.edu>
2024-07-23 02:48:57 -0700

    profile screen fully functional

--- App.js
+++ App.js
@@ -311,6 +311,11 @@ function Logger({ route, navigation }) {
   );
 
   function ResistanceScreen() {
+    // Handles input currently being typed
+    // i: index of workout, t: text buffer, f: field for workout (name, sets, etc)
+    const defaultCurrInput = {i: -1, t: "", f: ""};
+    const [currInput, setCurrInput] = useState(defaultCurrInput);
+
     return (
       <ScrollView>
         {/* Adds a new workout; appends to our array a new exercise with blank values initialized */}
@@ -318,14 +323,31 @@ function Logger({ route, navigation }) {
           <Text style={{ fontSize: 36, textAlign: 'center' }}>+</Text>
         </TouchableOpacity>
         {/* Iterates over workouts array with the View below for each workout */}
+        {/*
+          How input works so keyboard doesn't flicker
+          currInput: React State that acts as a buffer to type to
+          onFocus: When TextInput is first tapped, currInput is updated with the current
+          workout's index and text
+          onBlur: When TextInput is unfocused (user taps away from keyboard), value is updated
+          and rerendered
+          onChangeText: On every keystroke, currInput's text value is updated
+        */}
         {workouts.map((workout, index) => (
           <View key={index} style={styles.workoutEntry}>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
             <TextInput
               style={[styles.input, { flex: 1 }]}
               placeholder="Enter exercise name..."
-              value={workout.exerciseName}
-              onChangeText={(text) => handleValueChange(index, 'exerciseName', text)}
+              value={currInput.f === "name" && index === currInput.i ? currInput.t : workout.exerciseName}
+              onFocus={() => {
+                setCurrInput({i: index, t: workout.exerciseName, f: "name"});
+              }}
+              onBlur={() => {
+                  handleValueChange(index, 'exerciseName', currInput.t);
+                  setCurrInput(defaultCurrInput);
+                }
+              }
+              onChangeText={(text) => setCurrInput({i: index, t: text, f: "name"})}
             />
             <TouchableOpacity style={styles.deleteButton} onPress={() => deleteWorkout(index)}>
                 <Text style={styles.deleteButtonText}>Remove</Text>
@@ -336,22 +358,46 @@ function Logger({ route, navigation }) {
               <TextInput
                 style={styles.input2}
                 placeholder="Sets"
-                value={workout.sets}
-                onChangeText={(text) => handleValueChange(index, 'sets', text)}
+                value={currInput.f === "sets" && index === currInput.i ? currInput.t : workout.sets}
+                onFocus={() => {
+                  setCurrInput({i: index, t: workout.sets, f: "sets"});
+                }}
+                onBlur={() => {
+                    handleValueChange(index, 'sets', currInput.t);
+                    setCurrInput(defaultCurrInput);
+                  }
+                }
+                onChangeText={(text) => setCurrInput({i: index, t: text, f: "sets"})}
                 keyboardType="numeric"
               />
               <TextInput
                 style={styles.input2}
                 placeholder="Reps"
-                value={workout.reps}
-                onChangeText={(text) => handleValueChange(index, 'reps', text)}
+                value={currInput.f === "reps" && index === currInput.i ? currInput.t : workout.reps}
+                onFocus={() => {
+                  setCurrInput({i: index, t: workout.reps, f: "reps"});
+                }}
+                onBlur={() => {
+                    handleValueChange(index, 'reps', currInput.t);
+                    setCurrInput(defaultCurrInput);
+                  }
+                }
+                onChangeText={(text) => setCurrInput({i: index, t: text, f: "reps"})}
                 keyboardType="numeric"
               />
               <TextInput
                 style={styles.input2}
                 placeholder="Weight"
-                value={workout.weight}
-                onChangeText={(text) => handleValueChange(index, 'weight', text)}
+                value={currInput.f === "weight" && index === currInput.i ? currInput.t : workout.weight}
+                onFocus={() => {
+                  setCurrInput({i: index, t: workout.weight, f: "weight"});
+                }}
+                onBlur={() => {
+                    handleValueChange(index, 'weight', currInput.t);
+                    setCurrInput(defaultCurrInput);
+                  }
+                }
+                onChangeText={(text) => setCurrInput({i: index, t: text, f: "weight"})}
                 keyboardType="numeric"
               />
             </View>
@@ -363,6 +409,10 @@ function Logger({ route, navigation }) {
   }
 
   function CardioScreen() {
+    // Handles current input
+    const defaultCurrInput = {i: -1, t: "", f: ""};
+    const [currInput, setCurrInput] = useState(defaultCurrInput);
+
     return (
       <ScrollView>
         {/* Adds a new workout; appends to our array a new exercise with blank values initialized */}
@@ -376,8 +426,16 @@ function Logger({ route, navigation }) {
             <TextInput
               style={[styles.input, { flex: 1 }]}
               placeholder="Enter exercise name..."
-              value={cardio.cardioName}
-              onChangeText={(text) => handleCardioChange(index, 'cardioName', text)}
+              value={currInput.f === "name" && index === currInput.i ? currInput.t : cardio.cardioName}
+              onFocus={() => {
+                setCurrInput({i: index, t: cardio.cardioName, f: "name"});
+              }}
+              onBlur={() => {
+                  handleCardioChange(index, 'cardioName', currInput.t);
+                  setCurrInput(defaultCurrInput);
+                }
+              }
+              onChangeText={(text) => setCurrInput({i: index, t: text, f: "name"})}
             />
             <TouchableOpacity style={styles.deleteButton} onPress={() => deleteCardio(index)}>
                 <Text style={styles.deleteButtonText}>Remove</Text>
@@ -388,15 +446,31 @@ function Logger({ route, navigation }) {
               <TextInput
                 style={[styles.input2, {width: '50%'}]}
                 placeholder="Time"
-                value={cardio.time}
-                onChangeText={(text) => handleCardioChange(index, 'time', text)}
+                value={currInput.f === "time" && index === currInput.i ? currInput.t : cardio.time}
+                onFocus={() => {
+                  setCurrInput({i: index, t: cardio.time, f: "time"});
+                }}
+                onBlur={() => {
+                    handleCardioChange(index, 'time', currInput.t);
+                    setCurrInput(defaultCurrInput);
+                  }
+                }
+                onChangeText={(text) => setCurrInput({i: index, t: text, f: "time"})}
                 keyboardType="numeric"
               />
               <TextInput
                 style={[styles.input2, {width: '50%'}]}
                 placeholder="Distance"
-                value={cardio.distance}
-                onChangeText={(text) => handleCardioChange(index, 'distance', text)}
+                value={currInput.f === "distance" && index === currInput.i ? currInput.t : cardio.distance}
+                onFocus={() => {
+                  setCurrInput({i: index, t: cardio.distance, f: "distance"});
+                }}
+                onBlur={() => {
+                    handleCardioChange(index, 'distance', currInput.t);
+                    setCurrInput(defaultCurrInput);
+                  }
+                }
+                onChangeText={(text) => setCurrInput({i: index, t: text, f: "distance"})}
                 keyboardType="numeric"
               />
             </View>
@@ -416,29 +490,34 @@ function Logger({ route, navigation }) {
 function Profile() {
   
   //for setting stuff
+  const [profileData, setProfileData] = useState({
+    "name": "",
+    "weight": "",
+    "height": "",
+    "age": ""
+  });
   const [prs, setPRs] = useState({});
-  const [height, setHeight] = useState('');
-  const [weight, setWeight] = useState('');
-  const [age, setAge] = useState('');
-  const [name, setName] = useState('');
+  const keys = ["name", "weight", "height", "age"];
+
+  // Handles input currently being typed
+  // t: text buffer, f: field for profile (name, weight, etc)
+  const defaultCurrInput = {t: "", f: ""};
+  const [currInput, setCurrInput] = useState(defaultCurrInput);
+
   //loading profile from local storage
   useEffect(() => {
     const loadProfile = async () => {
-      //loading the stored data on the user when mounted
-      const storedHeight = await AsyncStorageUtils.getItem('height');
-      const storedWeight = await AsyncStorageUtils.getItem("weight");
-      const storedAge = await AsyncStorageUtils.getItem('age');
-      const storedName = await AsyncStorageUtils.getItem('name');
-      //validating we got an actual value and setting the value
-      if(storedAge) setAge(storedAge);
-      if(storedHeight) setHeight(storedHeight);
-      if(storedWeight) setWeight(storedWeight);
-      if(storedName) setName(storedName);
+      const prevData = { ...profileData };
+      for (const key in keys) {
+        const val = await AsyncStorageUtils.getItem(keys[key]);
+        if (val) {
+          console.log("Found field: " + val);
+          prevData[keys[key]] = val;
+        }
+      }
+      setProfileData(prevData);
     };
-    loadProfile();
 
-  }, );
-  useEffect(()=>{
     const loadPRs = async () => {
       try {
         const storedPRs = await AsyncStorageUtils.getItem('prs');
@@ -448,50 +527,83 @@ function Profile() {
       } catch (error) {
         console.error('Failed to load PRs:', error);
       }
-    };
-
+    }
+    console.log("Loading profile...");
+    loadProfile();
+    console.log("Profile loaded!");
+    console.log("Loading PRs...");
     loadPRs();
-  }, [prs] );
-  //saving profile to local storage
-  useEffect(() =>{
-    const saveProfile = async () => {
-     
-      await AsyncStorageUtils.setItem('height', height);
-      await AsyncStorageUtils.setItem('weight', weight);
-      await AsyncStorageUtils.setItem('age', age);
-      await AsyncStorageUtils.setItem('name', name);
-    };
+    console.log("PRs loaded!")
+  }, []);
 
+  const saveProfile = async () => {
+    console.log("Saving profile...");
+    for (const key in keys) {
+      if (profileData[keys[key]]) {
+        console.log(profileData[keys[key]]);
+        await AsyncStorageUtils.setItem(keys[key], profileData[keys[key]]);
+      }
+    }
+    console.log("Profile saved!");
+  };
+
+  const handleProfileChange = (key, value) => {
+    console.log("Changing profile...");
+    setProfileData((prevProfile) => {
+      if (value) prevProfile[key] = value;
+      return prevProfile;
+    });
+    console.log("New " + key + ": " + profileData[key]);
     saveProfile();
-  }, [height,weight,age,name]);
+    console.log("Profile changed!");
+  };
 
-  
   return(
     <ScrollView style = {styles.profileContainer}>
         <Text style= {styles.header}> Personal Information</Text>
         <TextInput
           placeholder = "Name"
-          value = {name}
-          onChangeText = {setName}
+          value={currInput.f === "name" ? currInput.t : profileData['name']}
+          onFocus={() => setCurrInput({t: profileData['name'], f: "name"})}
+          onBlur={() => {
+            handleProfileChange("name", currInput.t);
+            setCurrInput(defaultCurrInput);    
+          }}
+          onChangeText={(text) => setCurrInput({t: text, f: "name"})}
           style={styles.input}
           />
         <TextInput
           placeholder = "Weight"
-          value = {weight}
-          onChangeText = {setWeight}
+          value={currInput.f === "weight" ? currInput.t : profileData.weight}
+          onFocus={() => setCurrInput({t: profileData.weight, f: "weight"})}
+          onBlur={() => {
+            handleProfileChange("weight", currInput.t);
+            setCurrInput(defaultCurrInput);    
+          }}
+          onChangeText={(text) => setCurrInput({t: text, f: "weight"})}
           style={styles.input}
           />
         <TextInput
           placeholder = "Height"
-          value = {height}
-          onChangeText = {setHeight}
+          value={currInput.f === "height" ? currInput.t : profileData.height}
+          onFocus={() => setCurrInput({t: profileData.height, f: "height"})}
+          onBlur={() => {
+            handleProfileChange("height", currInput.t);
+            setCurrInput(defaultCurrInput);    
+          }}
+          onChangeText={(text) => setCurrInput({t: text, f: "height"})}
           style={styles.input}
           />
 
         <TextInput
           placeholder = "Age"
-          value = {age}
-          onChangeText = {setAge}
+          value={currInput.f === "age" ? currInput.t : profileData.age}
+          onFocus={() => setCurrInput({t: profileData.age, f: "age"})}
+          onBlur={() => {
+            handleProfileChange("age", currInput.t);
+            setCurrInput(defaultCurrInput);    
+          }}
+          onChangeText={(text) => setCurrInput({t: text, f: "age"})}
           style= {styles.input}
           />
 
